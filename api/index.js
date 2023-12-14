21c81b68bee27041905504caee883f8e03b35c3f
emiltonnhampossa <emiltonnhampossa0@gmail.com>
2023-12-14 23:08:35 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
+const mongoose = require('mongoose');
 const express = require('express')
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() =>{
+  console.log('conectado com sucesso no MongoDB')
+}).catch((err) =>{
+  console(err)
+})
+
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
